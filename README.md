b835b6835117ccc3e7cc01b50d6cb52fca3faaa2
Gregory Zhenin <gzhenin@schools.nyc.gov>
2019-04-03 17:58:06 -0400

    Initial commit of stand-alone fuzzy-searchbox component

---
+++ README.md
@@ -0,0 +1,39 @@
+# fuzzy-searchbox
+
+This component creates an accessible screen-reader-friendly autocomplete combobox with fuzzy searching.
+
+Dependencies:
+  - Fuse.js
+  - Lodash
+  - node-sass
+  - Vue
+
+Notes:
+  - This component expects a list of objects as input. ('items')
+  - A future update should allow items to be a list of strings as well in cases with simpler dropdown options
+  - An array of keys needs to be passed in so the component knows what keys to filter on. ('itemKeys')
+  - There are two options for specifying the labels for the items
+    1) 'itemLabels' can be passed in as an array of keys (just like 'itemKeys'). The label will be the concatenation of the key values. The delimiter can be passed in as 'labelDelimiter', otherwise the default is a single space.
+    2) 'createLabelFunction' can be passed in as a function that accepts an item and returns a string label.
+  - 'searchByLabel' is an optional flag to use the label string for search instead of using the itemKeys, useful when used with 'createLabelFunction'
+    - 'searchByLabelRuleFunction' is an optional parameter that can be passed in as a function that modifies the underlying search string
+  - 'preSelectedItem' should be set if there are events that pass an item selection into the FuzzySearchbox module from outside
+  - This compenent requires an aria-label to be set
+  - Optionally set 'placeholder' as text when the input box is empty
+
+  Sample usage:
+```html
+<FuzzySearchbox
+  id="my-select"
+  aria-label="select an option"
+  placeholder="Type here to search for an option"
+  v-model="selectedOption"
+  :preSelectedItem="defaultItem"
+  @change="optionChanged"
+  :items="selectList"
+  :item-keys="['name', 'type', 'category']"
+  :searchByLabel = "true"
+  :searchByLabelRuleFunction = "labelModificationFunction"
+  :create-label-function="displayStringForLabel">
+</FuzzySearchbox>
+```
---
+++ src/fuzzy-searchbox.vue
@@ -0,0 +1,402 @@
+<template>
+  <div
+    class="fuzzy-searchbox skiptranslate notranslate"
+    role="combobox"
+    aria-haspopup="listbox"
+    aria-owns="autocomplete-results"
+    :aria-expanded="isOpen"
+  >
+    <input
+      :placeholder="placeholder"
+      class="autocomplete-input"
+      type="text"
+      spellcheck="false"
+      @input="onChange"
+      v-model="searchTerm"
+      ref="input"
+      @keydown.down="onArrowDown"
+      @keydown.up="onArrowUp"
+      @keyup.enter="onEnter"
+      @keyup.escape="onLeave"
+      @focus="onClick"
+      @blur="onLeave"
+      aria-multiline="false"
+      role="searchbox"
+      aria-autocomplete="list"
+      aria-controls="autocomplete-results"
+      :aria-label="ariaLabel"
+      :aria-activedescendant="activedescendant"
+      autocomplete="off"
+      autocorrect="off"
+      autocapitalize="off"
+    />
+    <ul
+      id="autocomplete-results"
+      ref="autocomplete-results"
+      v-show="isOpen"
+      class="autocomplete-results"
+      role="listbox"
+    >
+      <li
+        class="loading"
+        v-if="isLoading"
+      >
+        Loading results...
+      </li>
+      <li
+        v-else
+        v-for="(result, i) in results"
+        :key="i"
+        @click="setResult(result)"
+        @mouseover="arrowCounter = i"
+        class="autocomplete-result"
+        :class="{ 'is-active': isSelected(i) }"
+        role="option"
+        :id="getId(i)"
+        :ref="getId(i)"
+        :aria-selected="isSelected(i)"
+      >
+      {{result.fuzzySearchboxLabel}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import Fuse from 'fuse.js'
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+import isEqual from 'lodash/isEqual'
+
+export default{
+  props: {
+    items: {
+      type: Array,
+      required: false,
+      default: () => []
+    },
+    isAsync: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isThrottled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isDebounced: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    ariaLabel: {
+      type: String,
+      required: true
+    },
+    itemKeys: {
+      type: Array,
+      required: false
+    },
+    itemLabels: {
+      type: Array,
+      required: false
+    },
+    labelDelimiter: {
+      type: String,
+      required: false,
+      default: ' '
+    },
+    createLabelFunction: {
+      type: Function,
+      required: false
+    },
+    searchByLabelRuleFunction: {
+      type: Function,
+      required: false
+    },
+    searchByLabel: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    preSelectedItem: [String, Object],
+    placeholder: {
+      type: String,
+      required: false
+    }
+  },
+
+  data () {
+    return {
+      isOpen: false,
+      results: [],
+      searchTerm: '',
+      isLoading: false,
+      arrowCounter: -1,
+      activedescendant: '',
+      selectedItem: {},
+      searchOptions: {
+        shouldSort: true,
+        tokenize: true,
+        matchAllTokens: true,
+        threshold: 0.5,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1
+      }
+    }
+  },
+
+  mounted () {
+    document.addEventListener('click', this.handleClickOutside)
+    if (this.searchByLabel) {
+      this.searchOptions.keys = this.labelSearchRuleExists ? ['fuzzySearchboxSearchLabel'] : ['fuzzySearchboxLabel']
+    } else {
+      this.searchOptions.keys = this.itemKeys
+    }
+    this.fuse = new Fuse(this.items, this.searchOptions)
+    if (this.preSelectedItem) {
+      this.selectedItem = this.preSelectedItem
+    }
+    this.throttledSearch = throttle(this.filterResults, 600, { 'leading': false })
+    this.debouncedSearch = debounce(this.filterResults, 600, { 'leading': false })
+  },
+
+  destroyed () {
+    document.removeEventListener('click', this.handleClickOutside)
+  },
+
+  methods: {
+    labelSearchRuleExists: function () {
+      return typeof this.searchByLabelRuleFunction === 'function'
+    },
+    onChange: function () {
+      this.isLoading = true
+      this.searchTerm = this.$refs['input'].value
+      if (!this.isAsync) {
+        this.isLoading = true
+        this.isThrottled ? this.throttledSearch() : this.isDebounced ? this.debouncedSearch() : this.filterResults()
+        this.arrowCounter = 0
+        this.isOpen = true
+        this.$refs['autocomplete-results'].scrollTop = 0
+      }
+    },
+    onClick: function () {
+      if (!this.isOpen) {
+        this.isOpen = true
+        this.$refs['input'].select()
+        if (this.results.length === 0) {
+          this.filterResults()
+        }
+      }
+    },
+    itemKeysSet: function () {
+      if (this.itemKeys) {
+        return true
+      } else {
+        return false
+      }
+    },
+    itemLabelsSet: function () {
+      if (this.itemLabels) {
+        return true
+      } else {
+        return false
+      }
+    },
+    filterResults: function () {
+      console.log('filterResults called')
+      if (this.searchTerm === '') {
+        this.results = this.items
+      } else {
+        var search = this.searchTerm
+        if (!this.searchOptions.matchAllTokens) {
+          if (search.substr(-1, 1) !== ' ') {
+            search = search + ' '
+          }
+        } else {
+          search = search.trim()
+        }
+        this.results = this.fuse.search(search)
+      }
+      this.isLoading = false
+    },
+    setResult: function (result) {
+      // this.search = this.itemKey ? result[this.itemKey] : result
+      this.selectedItem = result
+      this.isOpen = false
+      this.$emit('input', this.selectedItem)
+      this.$emit('change')
+    },
+    createLabel: function (item) {
+      var label = ''
+      if (typeof this.createLabelFunction === 'function') {
+        label = this.createLabelFunction(item)
+        return label
+      }
+      // Generic case
+      var keys = []
+      if (this.itemLabelsSet) {
+        keys = this.itemLabels
+      } else {
+        if (this.itemKeysSet) {
+          keys = this.itemKeys
+        } else {
+          keys = ['option'] // The key for arrays should be 'option'. This is yet to be implemented.
+        }
+      }
+      for (var key in keys) {
+        label += item[keys[key]] + this.labelDelimiter
+      }
+      label = label.substring(0, label.lastIndexOf(this.labelDelimiter))
+      return label
+    },
+    onArrowDown: function () {
+      if (this.isOpen) {
+        if (this.arrowCounter < this.results.length - 1) {
+          this.arrowCounter = this.arrowCounter + 1
+        }
+      }
+    },
+    onArrowUp: function () {
+      if (this.isOpen) {
+        if (this.arrowCounter > 0) {
+          this.arrowCounter = this.arrowCounter - 1
+        }
+      }
+    },
+    onEnter: function () {
+      if (this.isOpen) {
+        if (this.arrowCounter > -1) {
+          this.setResult(this.results[this.arrowCounter])
+          this.arrowCounter = -1
+        } else {
+          this.resetSearchTerm()
+        }
+        this.isOpen = false
+      }
+    },
+    onLeave: function () {
+      setTimeout(() => {
+        this.isOpen = false
+        this.arrowCounter = -1
+        this.resetSearchTerm()
+      }, 300)
+    },
+    handleClickOutside: function (evt) {
+      if (!this.$el.contains(evt.target) && this.isOpen) {
+        this.isOpen = false
+        this.arrowCounter = -1
+        this.resetSearchTerm()
+      }
+    },
+    resetSearchTerm: function () {
+      if (Object.keys(this.selectedItem).length !== 0) {
+        this.searchTerm = this.createLabel(this.selectedItem)
+      } else {
+        this.searchTerm = ''
+      }
+    },
+    setActiveDescendant: function () {
+      this.activedescendant = this.getId(this.arrowCounter)
+    },
+    getId: function (index) {
+      return `result-item-${index}`
+    },
+    isSelected: function (i) {
+      return i === this.arrowCounter
+    }
+  },
+
+  watch: {
+    items: function onItemsChange (val, oldValue) {
+      if (!isEqual(val, oldValue)) {
+        for (var itemCount in val) {
+          var item = val[itemCount]
+          item.fuzzySearchboxLabel = this.createLabel(item)
+          if (this.labelSearchRuleExists) {
+            item.fuzzySearchboxSearchLabel = this.searchByLabelRuleFunction(item.fuzzySearchboxLabel)
+          }
+        }
+        this.results = val
+        this.isLoading = false
+        this.fuse = new Fuse(val, this.searchOptions)
+      }
+    },
+    itemKeys: function onItemKeysChange (val, oldValue) {
+      if (!isEqual(val, oldValue)) {
+        if (this.searchByLabel) {
+          this.searchOptions.keys = this.labelSearchRuleExists ? ['fuzzySearchboxSearchLabel'] : ['fuzzySearchboxLabel']
+        } else {
+          this.searchOptions.keys = this.itemKeys
+        }
+        this.fuse = new Fuse(this.items, this.searchOptions)
+      }
+    },
+    selectedItem: function onSelectedItemChange (val, oldValue) {
+      this.searchTerm = val.fuzzySearchboxLabel
+    },
+    preSelectedItem: function onPreSelectedItemChange (val, oldValue) {
+      if (!isEqual(val, oldValue)) {
+        this.selectedItem = JSON.parse(JSON.stringify(this.preSelectedItem))
+      }
+    },
+    arrowCounter: function onArrowCounterChanged (val, oldValue) {
+      if (val >= 0) {
+        this.setActiveDescendant()
+        var target = this.$refs[this.getId(this.arrowCounter)][0]
+        var parent = target.parentNode
+        var targetVisible = target.offsetTop > parent.scrollTop && target.offsetTop < parent.scrollTop + parent.offsetHeight - target.offsetHeight
+        if (!targetVisible) {
+          if (val > oldValue) {
+            parent.scrollTop = target.offsetTop - parent.offsetHeight + target.offsetHeight
+          } else {
+            parent.scrollTop = target.offsetTop
+          }
+        }
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.fuzzy-searchbox {
+  position: relative;
+  display: inline-block;
+  width: inherit;
+}
+
+.autocomplete-results {
+  padding: 0;
+  margin: 0;
+  border: 1px solid #eeeeee;
+  max-height: 400px;
+  overflow-y: auto;
+  width: 100%;
+  left:0;
+  background-color: white;
+  position: absolute;
+  z-index: 999 !important;
+  box-shadow: 0 4px 5px rgba(0,0,0,.15);
+}
+
+.autocomplete-result {
+  list-style: none;
+  text-align: left;
+  padding: 4px 2px;
+  cursor: pointer;
+  display: list-item;
+}
+
+.autocomplete-result.is-active {
+  background-color: #4aae9b;
+  color: white;
+}
+
+.autocomplete-input {
+  width: inherit;
+}
+</style>
